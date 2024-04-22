66719b7573f47c9c15588ac1ac950ca4b4baed85
mazzocchi7053622 <167107766+mazzocchi7053622@users.noreply.github.com>
2024-04-22 12:18:46 +0200

    Delete cmake-build-release directory

--- cmake-build-release/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Marinella/Desktop/Siria/LAB_PROG_Mazzocchi/lib/googlemock
-# Build directory: C:/Users/Marinella/Desktop/Siria/LAB_PROG_Mazzocchi/cmake-build-release/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
